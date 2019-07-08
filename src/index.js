9feb4e624b766143541d55cf59d7c87638f6c224
pranalipatil209 <pranalipatil209@gmail.com>
2019-07-08 16:47:17 +0530

    ADD

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+	return <div>Hello React,Webpack 4 & Babel 7!</div>;
+};
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
